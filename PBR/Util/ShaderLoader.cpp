bb36f5c8feb29f0909b8dc6d7e5bb1ac8bf40fc0
bbtarzan12 <bbtarzan12@gmail.com>
2020-02-16 15:53:35 +0900

    Shader에 Include 기능 추가

--- PBR/Util/ShaderLoader.cpp
+++ PBR/Util/ShaderLoader.cpp
@@ -12,41 +12,14 @@ GLuint ShaderLoader::Load(const std::string& vertexFilePath, const std::string& 
 	GLuint VertexShaderID = glCreateShader(GL_VERTEX_SHADER);
 	GLuint FragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
 
-	// 버텍스 쉐이더 코드를 파일에서 읽기
-	std::string VertexShaderCode;
-	std::ifstream VertexShaderStream(vertexFilePath, std::ios::in);
-	if (VertexShaderStream.is_open())
-	{
-		std::stringstream sstr;
-		sstr << VertexShaderStream.rdbuf();
-		VertexShaderCode = sstr.str();
-		VertexShaderStream.close();
-	}
-	else
-	{
-		std::cout << "Can not read file " << vertexFilePath << std::endl;
-		return 0;
-	}
-
-	// 프래그먼트 쉐이더 코드를 파일에서 읽기
-	std::string FragmentShaderCode;
-	std::ifstream FragmentShaderStream(fragmentFilePath, std::ios::in);
-	if (FragmentShaderStream.is_open())
-	{
-		std::stringstream sstr;
-		sstr << FragmentShaderStream.rdbuf();
-		FragmentShaderCode = sstr.str();
-		FragmentShaderStream.close();
-	}
-
 	GLint Result = GL_FALSE;
 	int InfoLogLength;
 
-
 	// 버텍스 쉐이더를 컴파일
 	std::cout << "Compiling shader : " << vertexFilePath << std::endl;
-	char const * VertexSourcePointer = VertexShaderCode.c_str();
-	glShaderSource(VertexShaderID, 1, &VertexSourcePointer, NULL);
+	std::string vertexSourceCode = GetShaderCode(vertexFilePath);
+	const char* vertexSourcePointer = vertexSourceCode.c_str();
+	glShaderSource(VertexShaderID, 1, &vertexSourcePointer, nullptr);
 	glCompileShader(VertexShaderID);
 
 	// 버텍스 쉐이더를 검사
@@ -55,7 +28,7 @@ GLuint ShaderLoader::Load(const std::string& vertexFilePath, const std::string& 
 	if (InfoLogLength > 0)
 	{
 		std::vector<char> VertexShaderErrorMessage(InfoLogLength + 1);
-		glGetShaderInfoLog(VertexShaderID, InfoLogLength, NULL, &VertexShaderErrorMessage[0]);
+		glGetShaderInfoLog(VertexShaderID, InfoLogLength, nullptr, &VertexShaderErrorMessage[0]);
 		std::cout << &VertexShaderErrorMessage[0] << std::endl;
 	}
 
@@ -63,8 +36,9 @@ GLuint ShaderLoader::Load(const std::string& vertexFilePath, const std::string& 
 
 	// 프래그먼트 쉐이더를 컴파일
 	std::cout << "Compiling shader : " << fragmentFilePath << std::endl;
-	char const * FragmentSourcePointer = FragmentShaderCode.c_str();
-	glShaderSource(FragmentShaderID, 1, &FragmentSourcePointer, NULL);
+	std::string fragmentSourceCode = GetShaderCode(fragmentFilePath).c_str();
+	const char* fragmentSourcePointer = fragmentSourceCode.c_str();
+	glShaderSource(FragmentShaderID, 1, &fragmentSourcePointer, nullptr);
 	glCompileShader(FragmentShaderID);
 
 	// 프래그먼트 쉐이더를 검사
@@ -73,7 +47,7 @@ GLuint ShaderLoader::Load(const std::string& vertexFilePath, const std::string& 
 	if (InfoLogLength > 0)
 	{
 		std::vector<char> FragmentShaderErrorMessage(InfoLogLength + 1);
-		glGetShaderInfoLog(FragmentShaderID, InfoLogLength, NULL, &FragmentShaderErrorMessage[0]);
+		glGetShaderInfoLog(FragmentShaderID, InfoLogLength, nullptr, &FragmentShaderErrorMessage[0]);
 		std::cout << &FragmentShaderErrorMessage[0] << std::endl;
 	}
 
@@ -92,7 +66,7 @@ GLuint ShaderLoader::Load(const std::string& vertexFilePath, const std::string& 
 	if (InfoLogLength > 0)
 	{
 		std::vector<char> ProgramErrorMessage(InfoLogLength + 1);
-		glGetProgramInfoLog(ProgramID, InfoLogLength, NULL, &ProgramErrorMessage[0]);
+		glGetProgramInfoLog(ProgramID, InfoLogLength, nullptr, &ProgramErrorMessage[0]);
 		std::cout << &ProgramErrorMessage[0] << std::endl;
 	}
 
@@ -103,4 +77,61 @@ GLuint ShaderLoader::Load(const std::string& vertexFilePath, const std::string& 
 	glDeleteShader(FragmentShaderID);
 
 	return ProgramID;
+}
+
+std::string ShaderLoader::GetShaderCode(const std::string& path, const std::string& includeIdentifier /*= "#include "*/)
+{
+	static bool isRecursiveCall = false;
+
+	std::string fullSourceCode = "";
+	std::ifstream file(path);
+
+	if (!file.is_open())
+	{
+		std::cerr << "ERROR: could not open the shader at: " << path << "\n" << std::endl;
+		return fullSourceCode;
+	}
+
+	std::string lineBuffer;
+	while (std::getline(file, lineBuffer))
+	{
+		// Look for the new shader include identifier
+		if (lineBuffer.find(includeIdentifier) != lineBuffer.npos)
+		{
+			// Remove the include identifier, this will cause the path to remain
+			lineBuffer.erase(0, includeIdentifier.size());
+
+			// The include path is relative to the current shader file path
+			std::string pathOfThisFile;
+			getFilePath(path, pathOfThisFile);
+			lineBuffer.insert(0, pathOfThisFile);
+
+			// By using recursion, the new include file can be extracted
+			// and inserted at this location in the shader source code
+			isRecursiveCall = true;
+			fullSourceCode += GetShaderCode(lineBuffer, includeIdentifier);
+
+			// Do not add this line to the shader source code, as the include
+			// path would generate a compilation issue in the final source code
+			continue;
+		}
+
+		fullSourceCode += lineBuffer + '\n';
+	}
+
+	// Only add the null terminator at the end of the complete file,
+	// essentially skipping recursive function calls this way
+	if (!isRecursiveCall)
+		fullSourceCode += '\0';
+
+	file.close();
+
+	return fullSourceCode;
+}
+
+void ShaderLoader::getFilePath(const std::string& filePath, std::string& pathWithoutFileName)
+{
+	// Remove the file name and store the path to this folder
+	size_t found = filePath.find_last_of("/\\");
+	pathWithoutFileName = filePath.substr(0, found + 1);
 }
--- PBR/Util/ShaderLoader.h
+++ PBR/Util/ShaderLoader.h
@@ -7,4 +7,8 @@ class ShaderLoader
 {
 public:
 	static GLuint Load(const std::string& vertexFilePath, const std::string& fragmentFilePath);
+
+private:
+	static std::string GetShaderCode(const std::string& path, const std::string& includeIdentifier = "#include ");
+	static void getFilePath(const std::string& filePath, std::string& pathWithoutFileName);
 };
